0450ea42b9d04dfb6747d2ccf728e09c6175608c
Anuj Bhagat <anujbhagat2000@gmail.com>
2022-07-05 12:07:34 +0530

    Added

---
+++ EmployeeMicroservice/Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeMicroservice.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Gender { get; set; }
+        public string Designation { get; set; }
+        public int Points_Gained { get; set; }
+        public string Office_Location { get; set; }
+    }
+
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions options) : base(options)
+        { }
+
+        public DbSet<Employee> Employees { get; set; }  
+    }
+}
