6390bed70cd79e80739389d9c3ada77dbe4804ae
Ashish Sahu <ashishsahu10428@gmail.com>
2022-07-06 23:25:24 +0530

    Exception handing completed
    - Logging left

--- OffersMicroservices/OffersMicroservices/Controllers/OfferController.cs
+++ OffersMicroservices/OffersMicroservices/Controllers/OfferController.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using OffersMicroservices.Database;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -30,25 +30,45 @@ namespace OffersMicroservices.Controllers
              API to get all the offers     */
         [Route("Offer")]
         [HttpGet]
-        public IEnumerable<Offer> Offer()
+        public async Task<ActionResult<Offer>> Offer()
         {
-            return db.Offers.ToList();
+            try
+            {
+                var result = db.Offers.ToList();
+
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
+
 
 
         /*   GET: api/getOfferDetails/<offer_id>
              API to fetch data on the basis on offerID    */
         [Route("getOfferDetails/{id}")]
         [HttpGet]
-        public ActionResult<Offer> GetOfferDetails(int id)
+        public async Task<ActionResult<Offer>> GetOfferDetails(int id)
         {
             try
             {
-                return db.Offers.Find(id);
+                var result = await db.Offers.FindAsync(id);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
             }
-            catch (Exception e)
+            catch (Exception ex)
             {
-                return BadRequest(e.Message);
+                return BadRequest(ex.Message);
             }
         }
 
@@ -57,11 +77,16 @@ namespace OffersMicroservices.Controllers
              API to fetch filtered data on the basis of category    */
         [Route("getOfferDetailsByCategory/{id}")]
         [HttpGet]
-        public ActionResult<IEnumerable<Offer>> GetOfferDetailsByCategory(int id)
+        public async Task<ActionResult<IEnumerable<Offer>>> GetOfferDetailsByCategory(int id)
         {
             try
             {
-                return db.Offers.Where(offer => offer.Category_Id == id).ToList();
+                var result = db.Offers.Where(offer => offer.Category_Id == id).ToList();
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
             }
             catch (Exception e)
             {
@@ -74,11 +99,16 @@ namespace OffersMicroservices.Controllers
              API to fetch sorted data on the basis of likes    */
         [Route("getOfferByTopLikes")]
         [HttpGet]
-        public ActionResult<IEnumerable<Offer>> GetOfferByTopLikes()
+        public async Task<ActionResult<IEnumerable<Offer>>> GetOfferByTopLikes()
         {
             try
             {
-                return db.Offers.OrderByDescending(o => o.N_Likes).ToList();
+                var result = db.Offers.OrderByDescending(o => o.N_Likes).ToList();
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
 
             }
             catch (Exception e)
@@ -92,21 +122,21 @@ namespace OffersMicroservices.Controllers
              API to fetch sorted data on the basis of posted date    */
         [Route("getOfferByPostedDate")]
         [HttpGet]
-        public IEnumerable<Offer> GetOfferByPostedDate()
+        public async Task<ActionResult<Offer>> GetOfferByPostedDate()
         {
             try
             {
-                IEnumerable<Offer> result = db.Offers.OrderByDescending(o => o.Start_Date).ToList();
+                var result = db.Offers.OrderByDescending(o => o.Start_Date).ToList();
 
                 if (result == null)
                 {
-                    return (IEnumerable<Offer>)NotFound();
+                    return NotFound();
                 }
-                return result;
+                return Ok(result);
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                return (IEnumerable<Offer>)StatusCode(StatusCodes.Status500InternalServerError, "Error retriving data from the database");
+                return BadRequest(e.Message);
             }
 
         }
@@ -118,10 +148,22 @@ namespace OffersMicroservices.Controllers
              API to take an object of offer and store it in database    */
         [Route("addOffer")]
         [HttpPost]
-        public void CreateOffer(Offer offer)
+        public async Task<ActionResult> CreateOffer(Offer offer)
         {
-            var result = db.Offers.AddAsync(offer);
-            db.SaveChanges();
+            try
+            {
+                var result = await db.Offers.AddAsync(offer);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                db.SaveChanges();
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
 
 
@@ -129,29 +171,42 @@ namespace OffersMicroservices.Controllers
              API to edit offer data   */
         [Route("editOffer")]
         [HttpPost]
-        public void Edit(int Id, Offer data)
+        public async Task<ActionResult> Edit(int Id, Offer data)
         {
+            try
+            {
+                Offer temp = await db.Offers.FindAsync(Id);
+                if (temp == null)
+                {
+                    return NotFound();
+                }
+                if (temp.Title != null)
+                {
+                    temp.Title = data.Title;
+                }
+                if (temp.Description != null)
+                {
+                    temp.Description = data.Description;
+                }
+                if (temp.Start_Date != null)
+                {
+                    temp.Start_Date = data.Start_Date;
+                }
+                if (temp.End_Date != null)
+                {
+                    temp.End_Date = data.End_Date;
+                }
+                temp.Engaged_Date = def_date;
+                db.Offers.Update(temp);
+                db.SaveChanges();
+                return Ok(data);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
 
-            Offer temp = db.Offers.Find(Id);
-            if (temp.Title != null)
-            {
-                temp.Title = data.Title;
-            }
-            if (temp.Description != null)
-            {
-                temp.Description = data.Description;
-            }
-            if (temp.Start_Date != null)
-            {
-                temp.Start_Date = data.Start_Date;
-            }
-            if (temp.End_Date != null)
-            {
-                temp.End_Date = data.End_Date;
-            }
-            temp.Engaged_Date = def_date;
-            db.Offers.Update(temp);
-            db.SaveChanges();
+
         }
 
 
@@ -159,13 +214,27 @@ namespace OffersMicroservices.Controllers
              API fix engage_date of a specific offer_id to current date and time   */
         [Route("engageOffer")]
         [HttpPost]
-        public void Engage(int Id, int Emp_Id)
+        public async Task<ActionResult> Engage(int Id, int Emp_Id)
         {
-            Offer temp = db.Offers.Find(Id);
-            temp.Emp_Id = Emp_Id;
-            temp.Engaged_Date = System.DateTime.Now;
-            db.Offers.Update(temp);
-            db.SaveChanges();
+            try
+            {
+                Offer temp = await db.Offers.FindAsync(Id);
+                if (temp == null)
+                {
+                    return NotFound();
+                }
+                temp.Emp_Id = Emp_Id;
+                temp.Engaged_Date = System.DateTime.Now;
+                db.Offers.Update(temp);
+                db.SaveChanges();
+                return Ok(temp);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+
 
         }
 
@@ -175,11 +244,16 @@ namespace OffersMicroservices.Controllers
              API to fetch filtered data on the basis of EMP_ID    */
         [Route("getOfferDetailsByEmpID/{id}")]
         [HttpGet]
-        public ActionResult<IEnumerable<Offer>> GetOfferDetailsByEmpId(int id)
+        public async Task<ActionResult<IEnumerable<Offer>>> GetOfferDetailsByEmpId(int id)
         {
             try
             {
-                return db.Offers.Where(offer => offer.Emp_Id == id).ToList();
+                var result = db.Offers.Where(offer => offer.Emp_Id == id).ToList();
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
             }
             catch (Exception e)
             {
