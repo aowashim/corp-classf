4eccdd4f33c6d2ad69a816c610e041370827deaa
Raktim Das <63242090+raktimdas2238@users.noreply.github.com>
2022-07-06 23:16:49 +0530

    Added Points Controller

---
+++ PointsMicroservices/PointsMicroservices/Database/Entities/Point.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PointsMicroservices.Database.Entities
+{
+    public class Point
+    {
+        [Key]
+        public int Point_Id { get; set; }
+        public int Emp_Id { get; set; }
+        public int Emp_Point { get; set; }
+    }
+
+}
