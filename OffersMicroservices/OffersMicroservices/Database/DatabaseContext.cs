00ea5fd647cd884d7b01eb340d6f2be00d397c67
Owashim Akram <62115734+aowashim@users.noreply.github.com>
2022-07-08 15:42:01 +0530

    Changed GET controllers to include employee name

---
+++ OffersMicroservices/OffersMicroservices/Database/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OffersMicroservices.Database.Entities;
+
+namespace OffersMicroservices.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Offer> Offers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=owashim\sqlexpress;Initial Catalog=CorpClassf;Integrated Security=True");
+        }
+    }
+
+}
---
+++ OffersMicroservices/OffersMicroservices/Database/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OffersMicroservices.Database.Entities
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string Email { get; set; }
+        public string Designation { get; set; }
+        public int Points_Gained { get; set; }
+        public string Office_Location { get; set; }
+    }
+}
