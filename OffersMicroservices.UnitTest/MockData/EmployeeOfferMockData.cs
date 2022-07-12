dfcb38e00dba6c3e1fc132442dc7bf90dbf0fdd4
Raktim Das <63242090+raktimdas2238@users.noreply.github.com>
2022-07-12 17:20:11 +0530

    OfferMicroservices Testing Done

---
+++ OffersMicroservices.UnitTest/MockData/EmployeeOfferMockData.cs
@@ -0,0 +1,42 @@
+﻿using OffersMicroservice.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OffersMicroserviceUnitTest.MockData
+{
+    public static class EmployeeOfferMockData
+    {
+        public static List<EmployeeOffer> GetEmployeeOffers()
+        {
+            return new List<EmployeeOffer>()
+            {
+                new EmployeeOffer()
+                {
+                    id = 1,
+                    title = "Samsung E10",
+                    description = "Smartphone Good quality,  good condition",
+                    empName = "James",
+                    likes = 0
+                },
+                new EmployeeOffer()
+                {
+                    id = 2,
+                    title = "MSI Notebook",
+                    description = "8GB RAM, 1TB SSD",
+                    empName = "Raktim",
+                    likes = 0
+                },
+                new EmployeeOffer()
+                {
+                    id = 3,
+                    title = "DSA Book",
+                    description = "Author: Reema Thereja",
+                    empName = "Raktim",
+                    likes = 0
+                }
+            };
+        }
+    }
+}
---
+++ OffersMicroservices.UnitTest/MockData/OfferEmployeeMockData.cs
@@ -0,0 +1,88 @@
+﻿using OffersMicroservice.Repository;
+using OffersMicroservices.Database.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeMicroserviceUnitTest.MockData
+{
+    public static class OfferEmployeeMockData
+    {
+        public static List<OfferEmployee> GetOfferEmployee()
+        {
+            return new List<OfferEmployee>()
+            {
+                new OfferEmployee()
+                {
+                    id = 1,
+                    title = "Samsung E10",
+                    description = "Smartphone Good quality,  good condition",
+                    n_Likes = 50,
+                    start_Date = DateTime.Parse("2022-07-10 00:00:00.0000000"),
+                    end_Date = DateTime.Parse("2022-07-16 00:00:00.0000000"),
+                    engaged_Date = null,
+                    category_Id = 1,
+                    emp_Id = 325671,
+                    empName="Anuj"
+                },
+                new OfferEmployee()
+                {
+                    id = 2,
+                    title = "MSI NoteBook",
+                    description = "8GB RAM, 1 TB SSD",
+                    n_Likes = 10,
+                    start_Date = DateTime.Parse("2022-07-12 00:00:00.0000000"),
+                    end_Date = DateTime.Parse("2022-07-16 00:00:00.0000000"),
+                    engaged_Date = null,
+                    category_Id = 1,
+                    emp_Id = 325674,
+                    empName="Raktim"
+                },
+                new OfferEmployee()
+                {
+                    id = 3,
+                    title = "DSA Book",
+                    description = "Author: Reema Thereja",
+                    n_Likes = 70,
+                    start_Date = DateTime.Parse("2022-07-09 00:00:00.0000000"),
+                    end_Date = DateTime.Parse("2022-07-14 00:00:00.0000000"),
+                    engaged_Date = null,
+                    category_Id = 1,
+                    emp_Id = 325674,
+                    empName="Raktim"
+                }
+            };
+        }
+
+        //public static List<OfferEmployee> Offer()
+        //{
+        //    return new List<OfferEmployee>()
+        //    {
+        //        new OfferEmployee()
+        //        {
+        //            id = 3,
+        //            title = "Poco F3",
+        //            description = "Smartphone Good quality,  good condition",
+        //            empName = "Reshma",
+        //            likes = 15
+        //        },
+        //        new OfferEmployee()
+        //        {
+        //            id = 2,
+        //            title = "Poco F1",
+        //            description = "Smartphone Good quality,  good condition",
+        //            empName = "Reshma",
+        //            likes = 10
+        //        },
+        //        new OfferEmployee()
+        //        {
+        //            id = 1,
+        //            title = "Samsung E10",
+        //            description = "Smartphone Good quality,  good condition",
+        //            empName = "James",
+        //            likes = 5
+        //        }
+        //    };
+        //}
+    }
+}
